8d13704e92d707795f9b644d2f272e80c1ed4f49
wikidotedxe <07wikiarlianm@gmail.com>
2025-10-21 12:49:34 +0700

    Menambahkan project content

--- components/projects.tsx
+++ components/projects.tsx
@@ -13,24 +13,12 @@ interface ProjectCardProps {
   githubUrl?: string;
 }
 
-const ProjectCard = ({
-  title,
-  description,
-  image,
-  technologies,
-  liveUrl,
-  githubUrl,
-}: ProjectCardProps) => {
+const ProjectCard = ({ title, description, image, technologies, liveUrl, githubUrl }: ProjectCardProps) => {
   return (
     <div className="group relative flex flex-col overflow-hidden rounded-xl border border-accent transition-all hover:border-primary/50">
       {/* Project Image */}
-      <div className="relative h-64 overflow-hidden bg-accent">
-        <Image
-          src={image}
-          alt={title}
-          className="object-cover transition-transform duration-300 group-hover:scale-105"
-          fill
-        />
+      <div className="relative h-64 overflow-hidden bg-white flex items-center justify-center">
+        <Image src={image} alt={title} className="object-contain transition-transform duration-300 group-hover:scale-105 p-5 mt-2" fill />
       </div>
 
       {/* Content */}
@@ -58,11 +46,7 @@ const ProjectCard = ({
             </Button>
           )}
           {githubUrl && (
-            <Button
-              variant="outline"
-              className="rounded-full shadow-none"
-              asChild
-            >
+            <Button variant="outline" className="rounded-full shadow-none" asChild>
               <a href={githubUrl} target="_blank" rel="noopener noreferrer">
                 <GithubLogo className="mr-1 h-4 w-4" />
                 View Code
@@ -78,40 +62,68 @@ const ProjectCard = ({
 const Projects = () => {
   const projects = [
     {
-      title: "E-Commerce Platform",
-      description:
-        "A full-featured e-commerce platform with real-time inventory management, payment processing, and admin dashboard.",
-      image: "/placeholder.svg",
-      technologies: ["Next.js", "TypeScript", "Stripe", "Prisma", "PostgreSQL"],
-      liveUrl: "https://ecommerce-demo.com",
-      githubUrl: "https://github.com/username/ecommerce",
+      title: "Web App Landing page",
+      description: "Creating a WebApp-Based Company Profile for NoFileExistsHere, with content accessible through an admin panel and customizable as needed.",
+      image: "/images/project/page.png",
+      technologies: ["PHP", "Laravel", "Filament", "Livewire", "Mysql", "Redis", "Tailwind", "Bootstrap"],
+      liveUrl: "https://www.nofileexistshere.my.id/",
+      // githubUrl: "https://github.com/username/ecommerce",
     },
     {
-      title: "AI Task Manager",
-      description:
-        "Smart task management app that uses AI to categorize, prioritize, and suggest optimal task scheduling.",
-      image: "/placeholder.svg",
-      technologies: ["React", "Python", "TensorFlow", "FastAPI", "MongoDB"],
-      liveUrl: "https://ai-taskmanager.com",
-      githubUrl: "https://github.com/username/ai-taskmanager",
+      title: "Pesonal portfolio",
+      description: "Creating and updating a personal portfolio to be more modern and minimalist, with eye-catching animations.",
+      image: "/images/project/about.png",
+      technologies: ["React", "NextJS", "Tailwind", "NodeJS"],
+      liveUrl: "https://about.wikiarlianm.com",
+      githubUrl: "https://github.com/wikidotexe/portfolio-update",
     },
     {
-      title: "Real-time Chat Application",
-      description:
-        "Feature-rich chat application with real-time messaging, file sharing, and video calls.",
-      image: "/placeholder.svg",
-      technologies: ["React", "Socket.io", "WebRTC", "Node.js", "Redis"],
-      liveUrl: "https://chatapp-demo.com",
-      githubUrl: "https://github.com/username/chat-app",
+      title: "Landing Page",
+      description: "I created a responsive landing page for CV Berkah Teknik Mandiri, an engineering and construction services company.",
+      image: "/images/project/cvberkah.png",
+      technologies: ["PHP", "CSS", "Javascript", "Bootstrap", "Mysql"],
+      liveUrl: "https://cv-teknikmandiri.vercel.app/",
+      githubUrl: "https://github.com/wikidotexe/cv-teknikmandiri",
     },
     {
-      title: "AI Image Generator",
-      description:
-        "An AI image generator that uses a model to generate images based on a prompt.",
-      image: "/placeholder.svg",
-      technologies: ["React", "Next.js", "Tailwind CSS", "Shadcn UI"],
-      liveUrl: "https://ai-image-generator.com",
-      githubUrl: "https://github.com/username/ai-image-generator",
+      title: "Cloud Computing",
+      description: "Setting Up a Home Server self-hosted Using NextCloud with the Linux Ubuntu Server Operating System.",
+      image: "/images/project/cloud.png",
+      technologies: ["Nextcloud", "Docker", "Mysql", "Nginx", "Linux", "Cloudflare", "Redis"],
+      liveUrl: "https://nextcloud.com/instant-trial/",
+      // githubUrl: "https://github.com/nextcloud",
+    },
+    {
+      title: "Setup New Device Server",
+      description: "Deployed Mikrotik RB1100 HX2 at PT BON CAFE INDONESIA with 1Gbps speed and support for 50+ users, enabling smooth collaboration and file sharing.",
+      image: "/images/project/cafe.png",
+      technologies: ["Mikrotik", "Winbox", "Ruijie", "TP-Link", "Cisco Packet Tracer", "Windows Sever", "WHM", "Mail Server"],
+      // liveUrl: "",
+      // githubUrl: "",
+    },
+    {
+      title: "Installation Device Server",
+      description: "Deployed Mikrotik RB1100 HX2 at PT Smartec Teknologi Indonesia with 1Gbps speed, supporting 100+ users and seamless file sharing.",
+      image: "/images/project/smartec.png",
+      technologies: ["Mikrotik", "Winbox", "TP-Link", "Cisco Packet Tracer", "Wireshark"],
+      // liveUrl: "",
+      // githubUrl: "",
+    },
+    {
+      title: "Grafana, Network & Server Monitoring",
+      description: "Deployed Grafana with Prometheus, Node Exporter, and SNMP to monitor MikroTik networks and servers in real-time, ensuring optimal performance.",
+      image: "/images/project/grafana.png",
+      technologies: ["Mikrotik", "Winbox", "Docker", "Linux", "SNMP", "Synology", "Nginx Reverse Proxy", "Grafana", "Prometheus", "Node Exporter"],
+      // liveUrl: "",
+      // githubUrl: "",
+    },
+    {
+      title: "Mikrotik Hotspot Monitoring",
+      description: "Implemented and configured Synology NAS at PT Lizzie Parra Kreasi (BLP Beauty), delivering advanced storage solutions.",
+      image: "/images/project/mikhmon.png",
+      technologies: ["Mikrotik", "Winbox", "Rest-API", "Webserver", "Nginx Reverse Proxy", "SSL/TLS", "Certificate", "DHCP Server", "Port-forwarding"],
+      liveUrl: "https://mikhmon.comonet.id/admin.php?id=login",
+      githubUrl: "https://github.com/laksa19/mikhmonv3",
     },
   ];
 
@@ -122,12 +134,8 @@ const Projects = () => {
           <Badge variant="secondary" className="mb-4">
             Projects
           </Badge>
-          <h2 className="text-4xl sm:text-5xl font-bold tracking-tight">
-            Featured Work
-          </h2>
-          <p className="text-muted-foreground mt-2 sm:mt-4 text-lg">
-            Showcasing some of my best projects and technical achievements
-          </p>
+          <h2 className="text-4xl sm:text-5xl font-bold tracking-tight">Featured Work</h2>
+          <p className="text-muted-foreground mt-2 sm:mt-4 text-lg">Showcasing some of my best projects and technical achievements</p>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
